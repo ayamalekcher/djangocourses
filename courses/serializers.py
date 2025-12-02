1c3cfb7a8f75795dbf9a7c61c1d166e4edb58226
ayamalekcher <ayamalek.cherchar@univ-constantine2.dz>
2025-12-02 21:12:32 +0100

    initial commit

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from .models import Course, StudentCourse
+from .models import StudentCourse, Course, Student
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
 
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
 class StudentCourseSerializer(serializers.ModelSerializer):
+    student = StudentSerializer(read_only=True)
+    course = CourseSerializer(read_only=True)
+
     class Meta:
         model = StudentCourse
         fields = '__all__'
